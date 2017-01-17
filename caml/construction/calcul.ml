af971d19dea184e4120c22207a204cfc8747b03f
Noël Flantier <raphwormser@gmail.com>
2017-01-18 00:07:46 +0100

    Revert "je commence à compiler" Il fallait faire ce commit sur calcul-nombre et non pas sur gui... This reverts commit 4e6440c4cd98baf6ef2075e9333f1af23599f633.

--- caml/construction/calcul.ml
+++
@@ -1,7 +0,0 @@
-module Fl = Flexagones;;
-
-let a = Fl.flexagone_trois;;
-
-let b = ajoute_face 0 a;;
-
-
--- caml/construction/flexagones.mli
+++ caml/construction/flexagones.mli
@@ -6,39 +6,39 @@ Ici sont les opérations de base autorisées
 Ces fonctions sont là pour une implantation persistante.
 *)
 
-module Flexagones: sig
-  type flexagone
-         
-  (*
-Représente le flexagone d'ordre 3 à partir duquel on construit les autres
-   *)
-  val flexagone_trois : flexagone
-                          
-                          
-  (*
-Pour un flexagone f,  le nombre de faces de f.
-   *)
-  val nombre_faces: flexagone -> int
-                            
 
-  (*
+type flexagone
+
+(*
+Représente le flexagone d'ordre 3 à partir duquel on construit les autres
+ *)
+val flexagone_trois : flexagone
+
+
+(*
+Pour un flexagone f, ordre f est le nombre de faces du flexagone
+ *)
+val ordre: flexagone -> int
+
+
+(*
 Pour chaque flexagone f d'ordre n, il est possible d'ajouter un face 
 à exactement n positions différentes.
 Ces positions sont ordonnées, et ajoute_face flexagone k renvoie le 
 flexagone obtenu en ajoutant une face à la kième position disponible.
-   *)
-  val ajoute_face : flexagone -> int -> flexagone
+ *)
+val ajoute_face : flexagone -> k -> flexagone
 
 
-  (*
+(*
 Pour un entier k et un flexagone f, rotation k f renvoie f "trouné" de 2*k*pi/n rad
-   *)
-  val rotation: int -> flexagone -> flexagone
-                                    
-  (*
+ *)
+val tourne: int -> flexagone -> flexagone
+
+(*
 retourne f renvoie le symetrique du flexagone f
-   *)
-  val symetrie: flexagone -> flexagone
-                               
-                               
-end
+ *)
+val retourne: flexagone -> flexagone
+
+
+
