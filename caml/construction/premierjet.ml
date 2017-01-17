f19f3e06ff360406e98080d8d837b4e4eeea2288
Noël Flantier <raphwormser@gmail.com>
2017-01-17 23:16:20 +0100

    début des graphes par tableaux d'adjacence

--- caml/construction/premierjet.ml
+++ caml/construction/premierjet.ml
@@ -1,7 +1,17 @@
 (*
 Premier essai:
-implémentation de flexagones par des tableaux.
+Implémentation de flexagones par des tableaux.
+Implémetation impérative
  *)
+
+
+type 'a graphe = 'a list array;;
+
+type flexagone = int graphe;;
+
+
+let (ordre: flexagone -> int) =fun f -> Array.length f;;
+
 
 let array_rot_unit t =
   let n = Array.length t in
@@ -12,22 +22,23 @@ let array_rot_unit t =
   done;
   t.(0) <- !a;;
 
-let array_rot t k =
+let rotation k f =
   for i = 0 to k-1 do
-    array_rot_unit t;
+    array_rot_unit f;
   done;;
 
 let swap t i j = let a = ref t.(i) in
 		 t.(i) <- t.(j);
 		 t.(j) <- !a;;
-let array_refl t =
+
+let symetrie t =
   let n = Array.length t in
   for i = 0 to n/2-1 do
     swap t i (n-i-1);
   done;;
 
-
-
+let (f61: flexagone) = [|[1;5];[5;4;2;0];[1;3;4];[2;4];[3;2;1;5];[4;1;0]|];;
+rotation 3 f61;; 
 
 let diedral_equiv t1 t2 =
   let n1,n2 = Array.length t1,Array.length t2 in
