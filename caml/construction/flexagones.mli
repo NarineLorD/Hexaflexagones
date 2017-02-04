aa50f77b21f50183eea1f40733fec391ac7d4765
NarineLorD <raphwormser@gmail.com>
2017-02-04 18:55:19 +0100

    Update flexagones.mli
    correction d'un ":" manquant

--- caml/construction/flexagones.mli
+++ caml/construction/flexagones.mli
@@ -5,7 +5,7 @@ structure abstraite des flexagones
 type flexagone
 
 val ordre: flexagone -> int
-val ajoute_face flexagone -> (int*int) -> flexagone
+val ajoute_face: flexagone -> (int*int) -> flexagone
 val rotation: flexagone -> int -> flexagone
 val symetrie: flexagone -> flexagone
 val equiv: flexagone-> flexagone -> bool
