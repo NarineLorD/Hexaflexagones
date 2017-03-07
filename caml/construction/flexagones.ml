a5a8f707915e223a657395cbb8f33f2e9d7554ea
Noël Flantier <raphwormser@gmail.com>
2017-03-07 01:43:50 +0100

    corrigé bug flexagones

--- caml/construction/flexagones.ml
+++ caml/construction/flexagones.ml
@@ -6,6 +6,7 @@ C'est ce que j'essaye d'implémenter ici
  *)
 open Btree
 
+
 type flexagone = {ordre:int;
                   aretes:(int*int) Abr.abr}
 
@@ -26,42 +27,64 @@ let change_couleur_flexagone f x y =
 alors cette fonction doit renvoyer le flexagone g où x a été renommé en y.
 On pourrait appeler cette fonction change_couleur: change la couleur d'une face.
 Remarque: je ne me soucie pas de savoir si la couleur y est déjà utilisée dans f, faire attention à l'usage !*)
-  {ordre = ordre f; aretes = change_couleur (f.aretes) x y};;
+  {ordre = ordre f; aretes = change_couleur (f.aretes) x y}
 
 
 let ajoute_face f (x,y) =
   let n = ordre f in
-  assert (y=x+1 && y<n);
+  assert (y mod n = (x+1) mod n && y<n);
   let g = ref f in
   for i = n-1 downto y do
     g := change_couleur_flexagone !g i (i+1)
   done;
-  {ordre = n+1; aretes = Abr.add (!g.aretes) (x,y+1)};;
+  {ordre = n+1; aretes = Abr.add (!g.aretes) (x,y+1)}
 
   
   
 let rotation f k = 
   let n = ordre f in
-  let plus (x,y) = ((x+k) mod n, (y+k) mod n) in
+  let plus (x,y) = let a,b = (x+k) mod n, (y+k) mod n in (min a b, max a b) in
   {ordre = n; aretes = map f.aretes plus}
 
 let symetrie f = 
   let n = ordre f in
-  let sym (x,y) = (n-x, n-y) in
+  let sym (x,y) = (n-1-y, n-1-x) in
   {ordre=n; aretes=map f.aretes sym}
 
-let equiv f g = 
-(*cette fonction ne fonctionne pas (ne renvoie pas le bon résultat)*)
-(*à implémenter correctement: il suffit de prendre en compte les rotations/reflexions.*)
+
+
+let egal f g = 
   (ordre f = ordre g) && for_all (Abr.est_dans f.aretes)  g.aretes
 
+let equiv f g = 
+  let n = ordre f in
+  let t = ref (egal f g) in
+  let h = ref g in
+  for i = 0 to n do
+    h := rotation !h 1;
+    t := !t || egal !h f;
+  done;
+  h := symetrie !h;
+  for i = 0 to n do
+    t := !t || egal !h f;
+    h := rotation !h 1;
+  done;
+  !t
+                                 
 
 let check_rotation f k = 
-  for_all (Abr.est_dans f.aretes) (rotation f k).aretes
+  egal (rotation f k) f
 
 let check_symetrie f k = 
   let n = ordre f in
-  for_all (Abr.est_dans f.aretes) (map f.aretes (fun (x,y) -> ((n-x-k-k) mod n,(n-x-k-k) mod n)) )
+  for_all (Abr.est_dans f.aretes) (map f.aretes (fun (x,y) -> let a ,b = ((n-y-k-k) mod n,(n-x-k-k) mod n) in (min a b, max a b));;
+  
+  
+  
+let f4 () = ajoute_face (f3()) (0,1);;
 
-
-
+let a = ref (f4());;
+let b = ref (f4());;
+a := ajoute_face !a (0,1);;
+b := ajoute_face !b (1,2);;
+equiv !a !b;;
